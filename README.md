f28cbab1ee952e63a8301c2db68c89f9aa1497ba
RAVEN DELANEY <ravend17@gmail.com>
2019-09-16 14:28:56 -0700

    Started sales.html code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cookie-stand
+
+Salmon Cookies project
+Author: Raven Delaney
+
+Links and Resources:
+
+Reflections and Comments:
+
+Day 1(9/16/19):
+
---
+++ app.js
@@ -0,0 +1,8 @@
+var seattle = {
+  min : 23,
+  max : 65,
+  avg : 6.3
+}
+function random (min, max) {
+  return Math.floor(Math.random() * (max -min) + min;
+}
