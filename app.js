074f3317ddd7ab80df7c4842385f5329ec9601ec
RAVEN DELANEY <ravend17@gmail.com>
2019-09-16 15:00:47 -0700

    random function works

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+'use strict';
+
 var seattle = {
   min : 23,
   max : 65,
   avg : 6.3
-}
+};
 function random (min, max) {
-  return Math.floor(Math.random() * (max -min) + min;
+  return Math.floor(Math.random() * (max - min) + min);
 }
+var test = random(seattle.min, seattle.max);
+random(seattle.min, seattle.max);
+console.log(test);
