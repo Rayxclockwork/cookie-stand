462ceeaad64d5a1465f601934e76ca98dbebcdd3
RAVEN DELANEY <ravend17@gmail.com>
2019-09-16 15:37:57 -0700

    cookies per hour array

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Salmon Cookies project
 Author: Raven Delaney
 
 Links and Resources:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 Reflections and Comments:
 
--- app.js
+++ app.js
@@ -3,11 +3,22 @@
 var seattle = {
   min : 23,
   max : 65,
-  avg : 6.3
+  avg : 6.3,
+
+  salesHour : [],
+  custHour : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  avgCookies: function() {
+    for(var i=0; i < this.custHour.length; i++){
+      var sales = (random(this.min, this.max) * this.avg);
+      this.salesHour.push(sales);
+    }
+    console.log(this.salesHour);
+  },
 };
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
 function random (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-var test = random(seattle.min, seattle.max);
-random(seattle.min, seattle.max);
-console.log(test);
+
+seattle.avgCookies();
